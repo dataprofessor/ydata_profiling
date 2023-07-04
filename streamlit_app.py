8e65d66f36c42a5507e2c2756735235ef80fe318
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-07-04 17:38:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import pandas as pd
+import ydata_profiling
+from streamlit_pandas_profiling import st_profile_report
 
-st.title('🎈 App Name')
+st.title('🎈 `ydata_profiling`)
 
-st.write('Hello world!')
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+
+pr = df.profile_report()
+st_profile_report(pr)
