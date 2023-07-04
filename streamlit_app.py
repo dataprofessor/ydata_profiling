cafe2c108da4fa060db0d035606f855d8a5c4230
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-07-04 17:44:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import ydata_profiling
 from streamlit_pandas_profiling import st_profile_report
 
-st.title('🎈 `ydata_profiling`')
+st.title('🎈 ydata_profiling')
 
 df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
 
